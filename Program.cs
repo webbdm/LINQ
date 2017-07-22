b5f4661e0e15c83822104cdfec4dd7869481eb27
webbdm <gwebb762@gmail.com>
2017-07-22 14:01:11 -0500

    numbers

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            var LFruits = from fruit in fruits
+                          where fruit.StartsWith("L")
+                          select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine($"{fruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+
+            var fourAndSixMultiples = numbers.Where(number => (number % 4 == 0) || (number % 6 == 0)); 
+
+            foreach (int number in fourAndSixMultiples)
+            {
+                Console.WriteLine($"{number}");
+            }
+
+
+
+
+            //var fourSixMultiples = numbers.Where();
+        }
+    }
+}
