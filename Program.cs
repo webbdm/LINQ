185982fa665b3a7068099bd80750d058726f66c3
webbdm <gwebb762@gmail.com>
2017-07-23 18:07:05 -0500

    Counting

--- Program.cs
+++ Program.cs
@@ -29,17 +29,76 @@ namespace LINQ
             };
 
 
-            var fourAndSixMultiples = numbers.Where(number => (number % 4 == 0) || (number % 6 == 0)); 
+            var fourAndSixMultiples = numbers.Where(number => (number % 4 == 0) || (number % 6 == 0));
 
-            foreach (int number in fourAndSixMultiples)
+            // foreach (int number in fourAndSixMultiples)
+            // {
+            //     Console.WriteLine($"{number}");
+            // }
+
+            // Order these student names alphabetically, in descending order (Z to A
+            List<string> names = new List<string>()
             {
-                Console.WriteLine($"{number}");
-            }
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            var descend = names.OrderByDescending(name => name);
+
+            // foreach (string name in descend)
+            // {
+            //     Console.WriteLine($"{name}");
+            // }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> nummies = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            //var ascendingNumbers = nummies.OrderBy(number => number);
+
+            var orderedNums =
+                from numnum in nummies
+                orderby numnum
+                select numnum;
+
+            // foreach (int num in orderedNums)
+            // {
+            //     Console.WriteLine(num);
+            // }
+
+            // Output how many numbers are in this list
+            List<int> nums = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            //var numberCount = nums.Count();
+            Console.WriteLine(nums.Count());
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            Console.WriteLine($"${purchases.Sum()}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            Console.WriteLine("Max");    
+            Console.WriteLine(prices.Max());
 
 
 
-
-            //var fourSixMultiples = numbers.Where();
         }
     }
 }
