5286be1b97b172ab65e4bc296f0509c1662e3167
webbdm <gwebb762@gmail.com>
2017-07-24 19:00:46 -0500

    Finished first bank exercise

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ using System.Collections.Generic;
 
 namespace LINQ
 {
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -17,10 +31,10 @@ namespace LINQ
                           where fruit.StartsWith("L")
                           select fruit;
 
-            foreach (string fruit in LFruits)
-            {
-                Console.WriteLine($"{fruit}");
-            }
+            // foreach (string fruit in LFruits)
+            // {
+            //     Console.WriteLine($"{fruit}");
+            // }
 
             // Which of the following numbers are multiples of 4 or 6
             List<int> numbers = new List<int>()
@@ -94,8 +108,62 @@ namespace LINQ
                 879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
             };
 
-            Console.WriteLine("Max");    
-            Console.WriteLine(prices.Max());
+            // Console.WriteLine("Max");
+            // Console.WriteLine(prices.Max());
+
+            /*
+               Store each number in the following List until a perfect square
+               is detected.
+               Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+           */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            var numsUntilPerfectSquare =
+                wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 != 0);
+
+            // Console.WriteLine("Squaredo!");
+            // foreach (var num in numsUntilPerfectSquare)
+            // {
+            //     Console.WriteLine(num);
+            // }
+
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            var millionaires = from customer in customers
+                               where customer.Balance >= 1000000
+                               select customer;
+
+            Console.WriteLine("Millionaires");
+            foreach (var person in millionaires)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            var twoCommas = from person in millionaires
+                            group person by person.Bank into thing
+                            select new { Bank = thing.Key, NumCustomers = thing.Count() };
+
+            foreach (var bankie in twoCommas)
+            {
+                Console.WriteLine($"{bankie.Bank} has {bankie.NumCustomers} millionaires");
+            }
+
 
 
 
