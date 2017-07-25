fccd94dce83f6682ebdca352fd589a668edbc5ba
webbdm <gwebb762@gmail.com>
2017-07-24 21:08:37 -0500

    Complete

--- Program.cs
+++ Program.cs
@@ -131,37 +131,70 @@ namespace LINQ
             // }
 
             // Build a collection of customers who are millionaires
-            List<Customer> customers = new List<Customer>()
-            {
-                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
-            };
+            // List<Customer> customers = new List<Customer>()
+            // {
+            //     new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            //     new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            //     new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            //     new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            //     new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            //     new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            //     new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            //     new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            //     new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            //     new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            // };
 
-            var millionaires = from customer in customers
-                               where customer.Balance >= 1000000
-                               select customer;
+            // var millionaires = from customer in customers
+            //                    where customer.Balance >= 1000000
+            //                    select customer;
 
             Console.WriteLine("Millionaires");
-            foreach (var person in millionaires)
-            {
-                Console.WriteLine(person.Name);
-            }
+            // foreach (var person in millionaires)
+            // {
+            //     Console.WriteLine(person.Name);
+            // }
 
-            var twoCommas = from person in millionaires
-                            group person by person.Bank into thing
-                            select new { Bank = thing.Key, NumCustomers = thing.Count() };
+            // var twoCommas = from person in millionaires
+            //                 group person by person.Bank into thing
+            //                 select new { Bank = thing.Key, NumCustomers = thing.Count() };
 
-            foreach (var bankie in twoCommas)
+            // foreach (var bankie in twoCommas)
+            // {
+            //     Console.WriteLine($"{bankie.Bank} has {bankie.NumCustomers} millionaires");
+            // }
+
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+        };
+
+            // Create some customers and store in a List
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+
+
+            var bankPeople = from bank in banks
+                             join customer in customers on bank.Symbol equals customer.Bank
+                             select new { Bank = bank.Name, Name = customer.Name };
+
+             foreach (var customer in bankPeople)
             {
-                Console.WriteLine($"{bankie.Bank} has {bankie.NumCustomers} millionaires");
+                Console.WriteLine($"{customer.Name} stores their goodies at {customer.Bank}");
             }
 
 
